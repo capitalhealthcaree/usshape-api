e43ff3c91e228305a67ab6ae3afc37514c802e24
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-08-29 18:02:46 +0500

    add options

--- index.js
+++ index.js
@@ -11,19 +11,8 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://usshape.org");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+app.options("*", cors()); // Handle preflight requests
+
 // Database Connection
 mongoose.connect(
   "mongodb+srv://ppbackend:Web786786@healthcarecluster.yhawahg.mongodb.net/usshape?retryWrites=true&w=majority"
