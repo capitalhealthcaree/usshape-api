2493be35ad9e2afd0c5e850b3e9cc77a6f0d2575
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-08-29 17:45:34 +0500

    add additional headers

--- index.js
+++ index.js
@@ -11,17 +11,29 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
-//
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://usshape.org");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 // Database Connection
 mongoose.connect(
-	"mongodb+srv://ppbackend:Web786786@healthcarecluster.yhawahg.mongodb.net/usshape?retryWrites=true&w=majority"
+  "mongodb+srv://ppbackend:Web786786@healthcarecluster.yhawahg.mongodb.net/usshape?retryWrites=true&w=majority"
 );
 const db = mongoose.connection;
 db.on("connected", () => {
-	console.log("db connected");
+  console.log("db connected");
 });
 db.on("disconnected", (err, res) => {
-	console.log("db disconnected", err, "###", res);
+  console.log("db disconnected", err, "###", res);
 });
 
 //All Routes
@@ -29,5 +41,5 @@ app.use("/", routes);
 
 // Server
 app.listen(appPort, () => {
-	console.log(`Server running at http://localhost/:${appPort}/`);
+  console.log(`Server running at http://localhost/:${appPort}/`);
 });
